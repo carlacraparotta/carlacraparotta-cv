eba487bf6c9f11874bab4d84c03f33a4b813b950
Carla Craparotta <carlacraparotta@outlook.com>
2024-04-25 00:25:55 +0200

    Inizio portfolio

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+function App() {
+
+  return (
+    <div className="page">
+      <div className="title">Portfolio</div>
+      <div className="title2">CarlaCraparotta</div>
+
+      <button className="btn">default</button>
+      <button className="btn" disabled>default</button>
+      <button className="btn dark">dark</button>
+      <button className="btn danger">danger</button>
+      <button className="btn primary">primary</button>
+      <button className="btn accent">accent</button>
+      <button className="btn success">success</button>
+      <button className="btn success outline">outline</button>
+      <button className="btn outline">outline</button>
+      <button className="btn primary">base</button>
+      <button className="btn primary lg">lg</button>
+    </div>
+  )
+}
+
+export default App
